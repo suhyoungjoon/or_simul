c9d85c4fd4e1718188c12cbd4b76af616f561a39
Suhyoungjoon <tjdudwns@gmail.com>
2026-06-29 10:52:11 +0900

    infra: import PostgreSQL Flexible Server, database, and firewall rule into Terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,3 +10,44 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = false
 }
+
+resource "azurerm_postgresql_flexible_server" "main" {
+  name                = "psql-scheduling-optimizer"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  version  = "16"
+  zone     = "1"
+
+  administrator_login    = "schedadmin"
+  administrator_password = var.postgres_admin_password
+
+  storage_mb            = 32768
+  storage_tier          = "P4"
+  auto_grow_enabled      = false
+
+  sku_name = "B_Standard_B1ms"
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+
+  public_network_access_enabled = true
+
+  lifecycle {
+    ignore_changes = [zone]
+  }
+}
+
+resource "azurerm_postgresql_flexible_server_database" "scheduling_optimizer" {
+  name      = "scheduling_optimizer"
+  server_id = azurerm_postgresql_flexible_server.main.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_all" {
+  name             = "AllowAll_2026-6-28_10-37-59"
+  server_id        = azurerm_postgresql_flexible_server.main.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
